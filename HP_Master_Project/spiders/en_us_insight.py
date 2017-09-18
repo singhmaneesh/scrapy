ff23a9360215b48d9c77dba570334e0b03c6da81
Ahmed Ezz Eldin <ahmedezeldin.93@gmail.com>
2017-09-18 05:36:58 +0200

    Update and Add support for API

--- HP_Master_Project/spiders/en_us_insight.py
+++ HP_Master_Project/spiders/en_us_insight.py
@@ -3,25 +3,61 @@ import scrapy
 import json
 import urlparse
 import urllib
-import logging
+import re
 
 from HP_Master_Project.items import ProductItem
+from HP_Master_Project.spiders import BaseProductsSpider
 
-
-class EnUsInsightSpider(scrapy.Spider):
+class EnUsInsightSpider(BaseProductsSpider):
     name = 'en-us_insight'
-    allowed_domains = ['http://www.insight.com']
-    products_api = 'http://www.insight.com/insightweb/getProduct'
-    product_api = 'http://www.insight.com/insightweb/getProductInfo'
+    allowed_domains = ['insight.com']
+    products_api = 'https://www.insight.com/insightweb/getProduct'
+    product_api = 'https://www.insight.com/insightweb/getProductInfo'
     flag = False
 
-    def start_requests(self):
-        payload = json.dumps(self.get_next_products_payload(page=1))
-        return [scrapy.Request(url=self.products_api, method='POST', body=payload,
-                               headers={'Content-Type': 'application/json'},
-                               callback=self.parse_products_api)]
+    SEARCH_URL = 'https://www.insight.com/en_US/search.html?qtype=all&q={search_term}'
 
-    def parse_products_api(self, response):
+    API_URL = 'https://admin.metalocator.com/webapi/api/matchedretailerproducturls?Itemid=8343' \
+              '&apikey=f5e4337a05acceae50dc116d719a2875&username=fatica%2Bscrapingapi@gmail.com' \
+              '&password=8y3$u2ehu2e..!!$$&retailer_id={retailer_id}'
+
+
+    def __init__(self, *args, **kwargs):
+        self.current_page=0
+        super(EnUsInsightSpider, self).__init__(site_name=self.allowed_domains[0], *args, **kwargs)
+
+
+    def start_requests(self):
+        for request in super(EnUsInsightSpider, self).start_requests():
+            if not self.product_url:
+                request = request.replace(callback=self.parse_search)
+            if self.retailer_id:
+                request = request.replace(callback=self.parse)
+            yield request
+
+
+    def parse_search(self, response):
+        payload = json.dumps(self.get_next_products_payload(page=1))
+        self.current_page+=1
+        return [scrapy.Request(url=self.products_api, method='POST', body=payload,
+                               headers={'Content-Type': 'application/json'}, meta=response.meta)]
+
+
+    def _scrape_results_per_page(self, response):
+        result_per_page = None
+        try:
+            json_response = json.loads(response.body.decode("utf-8", "ignore"))
+        except TypeError as e:
+            self.logger.error(e.message + "Json respone cannot be parsed")
+        else:
+            try:
+                result_per_page = int(json_response["shown"])
+            except Exception as e:
+                self.logger.error(e.message)
+        return result_per_page
+
+
+    def _scrape_product_links(self, response):
         self.logger.info("Start parsing products response")
         try:
             json_response = json.loads(response.body.decode("utf-8", "ignore"))
@@ -30,20 +66,36 @@ class EnUsInsightSpider(scrapy.Spider):
         except Exception as e:
             self.logger.error(e.message)
         else:
-            if self.flag:
-                num_products = json_response["shown"]
+            try:
+                num_products = int(json_response["shown"])
+            except:
+                if json_response:
+                    for item in json_response:
+                        mfr_part_id = self.get_mfr_part_num_from_url(item["product_link"])
+                        payload = json.dumps(self.get_product_payload(json_response, mfr_part_id))
+                        meta = response.meta
+                        meta['fire'] = True
+                        product_request = scrapy.Request(url=self.product_api, method='POST', body=payload, meta=meta,
+                                                         headers={'Content-Type': 'application/json'},
+                                                         callback = self.parse, dont_filter = True)
+                        yield product_request, ProductItem()
+            else:
                 for i in range(num_products):
-                    payload = json.dumps(self.get_product_payload(json_response, i))
-                    yield scrapy.Request(url=self.product_api, method='POST', body=payload, dont_filter=True,
-                                         headers={'Content-Type': 'application/json'},
-                                         callback=self.parse_product_response)
-            self.flag = True
-            current_page = self.get_current_page(json_response)
-            payload = json.dumps(self.get_next_products_payload(page=current_page + 1))
-            yield scrapy.Request(url=self.products_api, method='POST', body=payload, dont_filter=True,
-                                 headers={'Content-Type': 'application/json'}, callback=self.parse_products_api)
+                    mfr_part_id = json_response["nugsProducts"][i]["manufacturerPartNumber"]
+                    payload = json.dumps(self.get_product_payload(json_response, mfr_part_id))
+                    meta = response.meta
+                    meta['fire'] = True
+                    product_request = scrapy.Request(url=self.product_api, method='POST', body=payload, dont_filter=True,
+                                                     headers={'Content-Type': 'application/json'},
+                                                     meta=meta, callback=self.parse, )
+                    yield product_request, ProductItem()
 
-    def parse_product_response(self, response):
+
+    def _scrape_total_matches(self, response):
+        if self.retailer_id:
+            data = json.loads(response.body)
+            return len(data)
+        total_matches = None
         try:
             json_response = json.loads(response.body.decode("utf-8", "ignore"))
         except TypeError as e:
@@ -51,10 +103,52 @@ class EnUsInsightSpider(scrapy.Spider):
         except Exception as e:
             self.logger.error(e.message)
         else:
-            return self.parse_product_item(json_response)
+            try:
+                total_matches = int(json_response["nugsHitCount"])
+            except:
+                return None
+        return total_matches
 
-    def parse_product_item(self, json_response):
-        product = ProductItem()
+
+    def _scrape_next_results_page_link(self, response):
+        next_page_request = None
+        try:
+            json_response = json.loads(response.body.decode("utf-8", "ignore"))
+        except TypeError as e:
+            self.logger.error(e.message + "Json respone cannot be parsed")
+        except Exception as e:
+            self.logger.error(e.message)
+        else:
+            current_page = self.get_current_page(self, json_response)
+            if current_page:
+                payload = json.dumps(self.get_next_products_payload(page=current_page+1))
+                next_page_request = scrapy.Request(url=self.products_api, method='POST', body=payload, dont_filter=True,
+                                               headers={'Content-Type': 'application/json'}, meta=response.meta)
+            else:
+                payload = json.dumps(self.get_next_products_payload(page=current_page + 1))
+                next_page_request = scrapy.Request(url=self.products_api, method='POST', body=payload, dont_filter=True,
+                                                   headers={'Content-Type': 'application/json'}, meta=response.meta)
+        return next_page_request
+
+
+    def _parse_single_product(self, response):
+        return self.parse_product(response)
+
+
+    def parse_product(self, response):
+        meta = response.meta.copy()
+        product = meta.get('product', ProductItem())
+        try:
+            json_response = json.loads(response.body.decode("utf-8", "ignore"))
+        except TypeError as e:
+            self.logger.error(e.message + "Json respone cannot be parsed")
+        except Exception as e:
+            self.logger.error(e.message)
+        else:
+            return self.parse_product_item(json_response, product)
+
+
+    def parse_product_item(self, json_response, product):
         product["name"] = json_response["webProduct"]["description"]
         product["link"] = self.get_product_url(json_response)
         product["image"] = json_response["webProduct"]["image"]["largeImage"]
@@ -67,7 +161,7 @@ class EnUsInsightSpider(scrapy.Spider):
         product["manufacturer"] = json_response["webProduct"]["manufacturerName"]
         product["brand"] = json_response["webProduct"]["manufacturerName"]
         product["features"] = self.get_product_features(json_response)
-        product["retailer_key"] = json_response["webProduct"]["manufacturerPartNumber"]
+        product["retailer_key"] = self.retailer_id
         product["sku"] = json_response["webProduct"]["materialId"]
         product["ean"] = json_response["webProduct"]["unspscCode"]
         product["unspsc"] = json_response["webProduct"]["unspscCode"]
@@ -92,6 +186,7 @@ class EnUsInsightSpider(scrapy.Spider):
                                  json_response["webProduct"]["extendedSpecsMap"][specs]["details"][spec]["value"]})
         return features
 
+
     def get_product_url(self, json_response):
         insight_part_id = json_response["webProduct"]["insightPartNumber"]
         mfr_name = json_response["webProduct"]["manufacturerName"]
@@ -100,13 +195,23 @@ class EnUsInsightSpider(scrapy.Spider):
         return self.parse_product_url(insight_id=insight_part_id, mfr_id=mfr_part_id,
                                       mfr_name=mfr_name, product_name=product_name)
 
-    @staticmethod
-    def get_current_page(json_response):
-        return json_response["currentPage"]
+    def get_mfr_part_num_from_url(self, url):
+        mfr_part_id = re.findall(r'/[a-zA-Z0-9%]+/', url)[1].replace('/','').replace('%23','#')
+        return mfr_part_id
+
 
     @staticmethod
-    def get_product_payload(json_response, i):
-        mfr_part_id = json_response["nugsProducts"][i]["manufacturerPartNumber"]
+    def get_current_page(self, json_response):
+        current_page = None
+        try:
+            current_page = json_response["currentPage"]
+        except Exception as e:
+            self.logger.error(e.message)
+        return current_page
+
+
+    @staticmethod
+    def get_product_payload(json_response, mfr_part_id):
         payload = {"defaultPlant": "10",
                    "loadAccessories": False,
                    "cartFlag": False,
@@ -120,6 +225,7 @@ class EnUsInsightSpider(scrapy.Spider):
                    "contractId": None}
         return payload
 
+
     @staticmethod
     def parse_product_url(insight_id, mfr_id, mfr_name, product_name):
         base_url = u'http://www.insight.com/en_US/buy/product/'
@@ -132,6 +238,7 @@ class EnUsInsightSpider(scrapy.Spider):
         product_path = product_name.replace(' ', '-')
         product_url = quoted_url + '/' + product_path + '/'
         return product_url
+
 
     @staticmethod
     def get_next_products_payload(page, search_keyword="HP"):
