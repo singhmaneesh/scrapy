494969a6e96f63a642127cdaa12654173eb41ee4
Aabesh Karmacharya <aabesh.karmacharya@gmail.com>
2018-07-04 00:02:14 +0545

    used itemId as catentryId instead of product_id

--- HP_Master_Project/spiders/hp.py
+++ HP_Master_Project/spiders/hp.py
@@ -168,7 +168,7 @@ class HpSpider(BaseProductsSpider):
     @staticmethod
     def _get_stock_request(response):
         html = response.text
-        catentry_id = re.findall('var productId = \'(\d+?)\';', html)[0]
+        catentry_id = re.findall('data-a2c=\'{"itemId":"(\d+?)",', html)[0]
         store_id = re.findall('var storeId = \'(\d+?)\';', html)[0]
         lang_id = re.findall('var langId = \'(.+?)\';', html)[0]
         catalog_id = re.findall('var catalogId = \'(\d+?)\';', html)[0]
@@ -176,8 +176,9 @@ class HpSpider(BaseProductsSpider):
         return url.format(lang_id, store_id, catalog_id, catentry_id)
 
     def _parse_stock_status(self, response):
-        data = json.loads(response.text)
+        stock_value = self.STOCK_STATUS['CALL_FOR_AVAILABILITY']
         try:
+            data = json.loads(response.text)
             if data['priceData'][0]['price'] == '0.00':
                 stock_value = self.STOCK_STATUS['OUT_OF_STOCK']
             else:
@@ -186,7 +187,7 @@ class HpSpider(BaseProductsSpider):
                 else:
                     stock_value = self.STOCK_STATUS['IN_STOCK']
         except:
-            stock_value = self.STOCK_STATUS['OUT_OF_STOCK']
+            stock_value = self.STOCK_STATUS['CALL_FOR_AVAILABILITY']
         product = response.meta['product']
         product['productstockstatus'] = stock_value
         yield product
